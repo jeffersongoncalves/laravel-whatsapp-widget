a1baac2498e546b1bb5b33ffed918f459a8b42a8
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-12-24 18:14:20 -0300

    feat(models): add PHPDoc annotations to WhatsappAgent model
    Add property and method annotations to improve IDE support and static analysis.

--- src/Models/WhatsappAgent.php
+++ src/Models/WhatsappAgent.php
@@ -6,6 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 
+/**
+ * @property int $id
+ * @property bool $active
+ * @property string $name
+ * @property string $phone
+ * @property string|null $text
+ * @property string|null $image
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read string|null $image_url
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent whereText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|WhatsappAgent whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class WhatsappAgent extends Model
 {
     protected $fillable = [
